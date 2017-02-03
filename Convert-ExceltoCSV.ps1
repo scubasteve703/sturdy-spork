b1f4995a191bdc116b83c52fe81c57dab7739be2
scubasteve703 <steve.brennan@hotmail.com>
2017-02-03 10:53:13 -0500

    Uploading current scripts
    These need to be dissected and updated to work with controller script.

---
+++ Convert-ExceltoCSV.ps1
@@ -0,0 +1,16 @@
+﻿Function ExportWSToCSV ($excelFileName, $csvLoc)
+{
+    $excelFile = "C:\users\sbrennan\downloads\" + $excelFileName + ".xlsx"
+    $E = New-Object -ComObject Excel.Application
+    $E.Visible = $false
+    $E.DisplayAlerts = $false
+    $wb = $E.Workbooks.Open($excelFile)
+    foreach ($ws in $wb.Worksheets)
+    {
+        $n = $excelFileName + "_" + $ws.Name
+        $ws.SaveAs($csvLoc + $n + ".csv", 6)
+    }
+    $E.Quit()
+    Stop-Process -Name excel
+}
+#ExportWSToCSV -excelFileName "file" -csvLoc "C:\users\sbrennan\downloads\"
---
+++ Format-BrochureRequests.ps1
@@ -0,0 +1,20 @@
+﻿$csv=""
+$folder = "c:\users\sbrennan\downloads\"
+$filename = Read-Host -Prompt "enter filename:"
+#$producer = Read-host -Prompt "enter name of producer:"
+$path = $folder + $filename
+$date = get-date (get-date).AddDays(-1) -UFormat "%m%d"
+$outpath= "\\wccldfps\sbrennan\act lists\Brochure Requests\WCR BR"+" "+$date+".csv"
+$csv = import-csv -path $path
+$csv |
+Select-Object *, `
+@{expression={$_.'Conference Code'};label='Home Address 1'},`
+@{expression={$_.'Direct Phone'};label='Phone'},`
+@{expression={$_.'Direct Fax'};label='fax'},`
+@{expression={$_.'First Name' +' '+$_.'Last Name'};label='contact'},`
+@{expression={'x'};label='personal email'}`
+-ExcludeProperty 'conference code', 'direct phone', 'direct fax',date |
+Sort-Object -Property 'last name' |
+Export-Csv -Path $outpath -NoTypeInformation
+$csv.count
+ii $outpath
