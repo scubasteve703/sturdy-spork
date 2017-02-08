af81d054b46cf54c916a443c2cdc29135631e968
scubasteve703 <steve.brennan@hotmail.com>
2017-02-08 10:02:20 -0500

    New header type
    adding the AltMist header type also some changes to format-list and
    format-br

---
+++ Format-AltMist.ps1
@@ -0,0 +1,7 @@
+﻿ $outpath= "\\wccldfps\sbrennan\act lists\"+$marketer +"\"+$filename
+ $columns=(get-content "c:\users\sbrennan\documents\format-list\schema\AltMist.txt").split(",")
+            $csv = import-csv -path $path |select $columns|select *,@{expression={$_.phone};label='main number'},`
+            @{expression={$_.D_Phone};label='phone number'},@{expression={$_.d_fax};label='Fax'},`
+            @{expression={$_.person_ID};label='person ID'},@{expression={$_.keycode};label='messenger id'},`
+            @{expression={$_.firstname + ' ' +$_.lastname};label='contact'},@{expression={'x'};label='personal email'}`
+            -ExcludeProperty phone,d_phone,d_fax,person_id,keycode|Export-Csv $outpath -NoTypeInformation
